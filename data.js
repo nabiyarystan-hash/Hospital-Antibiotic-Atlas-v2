051052b03b27185adeea90329a14f7c6629c28ea
nabiyarystan-hash <nabiy.arystan@nu.edu.kz>
2026-07-29 15:42:16 +0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,42 @@
+const antibiotics = [
+  {
+    name: "Amoxicillin",
+    map: "https://datawrapper.dwcdn.net/wU9FJ/5/"
+  },
+  {
+    name: "Ceftriaxone",
+    map: "https://datawrapper.dwcdn.net/6Ff4p/3/"
+  },
+  {
+    name: "Ciprofloxacin",
+    map: "https://datawrapper.dwcdn.net/x5jjt/1/"
+  },
+  {
+    name: "Cefazolin",
+    map: "https://datawrapper.dwcdn.net/RAWLL/2/"
+  },
+  {
+    name: "Azithromycin",
+    map: "https://datawrapper.dwcdn.net/ZxZEi/1/"
+  },
+  {
+    name: "Levofloxacin",
+    map: "https://datawrapper.dwcdn.net/ZcVrk/1/"
+  },
+  {
+    name: "Gentamicin",
+    map: "https://datawrapper.dwcdn.net/cqeH8/1/"
+  },
+  {
+    name: "Doxycycline",
+    map: "https://datawrapper.dwcdn.net/zor1A/1/"
+  },
+  {
+    name: "Ampicillin",
+    map: "https://datawrapper.dwcdn.net/atoKM/1/"
+  },
+  {
+    name: "Amoxicillin/Clavulanate",
+    map: "https://datawrapper.dwcdn.net/z9KDz/1/"
+  }
+];
